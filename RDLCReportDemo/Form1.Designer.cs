d8527a550a68621b577faa807415e211658d6714
SaLaaR HuSyN <shsallu35@gmail.com>
2019-05-01 12:42:23 -0700

    Initial commit

---
+++ RDLCReportDemo/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace RDLCReportDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.DsSales = new RDLCReportDemo.DsSales();
+            this.tbl_FullSalesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbl_FullSalesTableAdapter = new RDLCReportDemo.DsSalesTableAdapters.tbl_FullSalesTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.DsSales)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbl_FullSalesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.tbl_FullSalesBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "RDLCReportDemo.SalesReport.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(655, 497);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // DsSales
+            // 
+            this.DsSales.DataSetName = "DsSales";
+            this.DsSales.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tbl_FullSalesBindingSource
+            // 
+            this.tbl_FullSalesBindingSource.DataMember = "tbl_FullSales";
+            this.tbl_FullSalesBindingSource.DataSource = this.DsSales;
+            // 
+            // tbl_FullSalesTableAdapter
+            // 
+            this.tbl_FullSalesTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 497);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DsSales)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbl_FullSalesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource tbl_FullSalesBindingSource;
+        private DsSales DsSales;
+        private DsSalesTableAdapters.tbl_FullSalesTableAdapter tbl_FullSalesTableAdapter;
+    }
+}
+
---
+++ RDLCReportDemo/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RDLCReportDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'DsSales.tbl_FullSales' table. You can move, or remove it, as needed.
+            this.tbl_FullSalesTableAdapter.Fill(this.DsSales.tbl_FullSales);
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
